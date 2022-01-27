4ef04195157f10e57069f3b7caff8859bd63c9bb
pungtz <90349672+pungtz@users.noreply.github.com>
2022-01-27 18:53:32 -0500

    Update tp2.cpp

--- TP2/tp2.cpp
+++ TP2/tp2.cpp
@@ -1,5 +1,29 @@
 #include <avr/io.h>
+#define F_CPU 8000000U
+#include <util/delay.h>
+#include <stdint.h>
 
+//Énumérer les entrées et sorties
+DDRA = 0xff;
+DDRD = 0x00;
+
+portA = 1 << 0;
+portD2 = 0x04;
+portD3 = 0x08;
+
+
+  
+enum state class (
+    OFF;
+    MID1;
+    MID2;
+    ON;
+)
+
+for (;;)
+{
+    case OFF;
+    
 int main()
 {
     
